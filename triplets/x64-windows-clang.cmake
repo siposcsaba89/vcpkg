94419dd7e933c9fe8ca3fe12c65d1e134a87f79e
Csaba Sipos <siposcsaba89@gmail.com>
2020-05-08 15:05:03 +0200

    clang triplet

---
+++ triplets/x64-windows-clang.cmake
@@ -0,0 +1,11 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_POLICY_SKIP_ARCHITECTURE_CHECK "enabled")
+if(DEFINED VCPKG_PLATFORM_TOOLSET) 
+# Silly trick to switch platform toolset to llvm while making sure vcpkg does what it normally does 
+    set(VCPKG_PLATFORM_TOOLSET llvm)
+endif()
+set(ENV{CXX} "E:/tools/LLVM/bin/clang-cl.exe") # for ninja
+set(ENV{CC} "E:/tools/LLVM/bin/clang-cl.exe")
+
