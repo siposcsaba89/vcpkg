322c5f9a70afeb423a730c8e7235e3d7f3742426
Csaba Sipos <siposcsaba89@gmail.com>
2021-06-09 11:23:38 +0200

    llvm triplet

--- triplets/x64-windows-clang.cmake
+++ triplets/x64-windows-clang.cmake
@@ -6,6 +6,6 @@ if(DEFINED VCPKG_PLATFORM_TOOLSET)
 # Silly trick to switch platform toolset to llvm while making sure vcpkg does what it normally does 
     set(VCPKG_PLATFORM_TOOLSET llvm)
 endif()
-set(ENV{CXX} "E:/tools/LLVM/bin/clang-cl.exe") # for ninja
-set(ENV{CC} "E:/tools/LLVM/bin/clang-cl.exe")
+set(ENV{CXX} "E:/tools/LLVM12/bin/clang-cl.exe") # for ninja
+set(ENV{CC} "E:/tools/LLVM12/bin/clang-cl.exe")
 
