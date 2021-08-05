59dc4b3177d11833374268581cdde5600e3e2676
Your Name <you@example.com>
2021-08-05 15:31:09 +0200

    [socket-io-client] use faulhornlabs github

--- ports/socket-io-client/portfile.cmake
+++ ports/socket-io-client/portfile.cmake
@@ -2,18 +2,20 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO socketio/socket.io-client-cpp
-    REF 3.0.0
-    SHA512 42735d73d24546b37332d649a2633f4a1b6e004b016c45d53bd8e230a157753bb319c80a59721865b9c3dcc588b4eec3cdf4ae9f7fc2cdf290b6bb07c866552c
+    REPO faulhornlabs/socket.io-client-cpp
+    REF 6b102024c54df0369c1c6d38bb9c8cc63bb98ff4
+    SHA512 0716b7585e04530e5578f51d07aa75e5ee8cfbad8e357f2778b7aab9a7af407351f055dc9f186dbfa6463fd462a813f78bbc4319964ecba033ad5025d4450816
     HEAD_REF master
-    PATCHES
-        fix-file-not-found.patch
-        fix-error-C3321.patch
+#    PATCHES
+#        fix-file-not-found.patch
+#        fix-error-C3321.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DBUILD_UNIT_TESTS=0
 )
 
 vcpkg_install_cmake()
